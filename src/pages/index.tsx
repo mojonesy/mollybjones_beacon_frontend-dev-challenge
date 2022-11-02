1c03b121c31c569d907342dd6b9672b6402d47e2
Molly <mojonesycreative@gmail.com>
2022-11-02 18:01:16 -0400

    logo, title, search bar jsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
 import styles from '../styles/Home.module.css'
+import Logo from '../assets/Logo';
+import Search from '../assets/Search';
 
 export default function Home() {
+
   return (
-    <div></div>
+    <div className={styles.container}>
+
+      {/* LOGO */}
+      <div className={styles.logo}>
+        <Logo />
+        <div className={styles.logotext}>
+          BEACON
+        </div>
+      </div>
+
+      <div className={styles.main}>
+      {/* Search */}
+        <h1 className={styles.title}>Pick Your School</h1>
+        <div className={styles.search}>
+          <div className={styles.eyeglass}>
+            <Search />
+          </div>
+          <div className={styles.searchtext}>
+            Search for your school
+          </div>
+        </div>
+        <div className={styles.searchbarspan}></div>
+
+        {/* List */}
+      </div>
+
+    </div>
   )
 }
