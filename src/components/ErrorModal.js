2cf66ae9d284335482ccd141be0db849f2ea4c9a
Molly <mojonesycreative@gmail.com>
2022-11-03 14:00:45 -0400

    Error modal

---
+++ src/components/ErrorModal.js
@@ -0,0 +1,24 @@
+import { Icon } from '@iconify/react';
+
+function ErrorModal({ styles, error, setError, setShowModal }) {
+    const handleClick = () => {
+        setError(null);
+        setShowModal(false);
+    }
+
+    return (
+        <div className={styles.errorModal}>
+            {error && <p className={styles.errorModalText}>{error}</p>}
+
+          <button 
+            className={styles.closeModalBtn}
+            onClick={handleClick}
+            aria-label='toggle error modal'
+          >
+            <Icon icon='gg:close' />
+          </button>
+        </div>
+    );
+}
+
+export default ErrorModal;
