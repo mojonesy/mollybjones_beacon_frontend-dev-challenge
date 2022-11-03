99f9cb55fbd0d5f269b2a8b73a8577bf0d0ef01b
Molly <mojonesycreative@gmail.com>
2022-11-02 21:24:30 -0400

    container styles in main app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app'
+import styles from '../styles/Home.module.css';
 
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.container}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
